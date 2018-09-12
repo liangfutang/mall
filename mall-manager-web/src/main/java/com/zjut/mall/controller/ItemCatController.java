940942f475fd84c9dff301e7a17834d0a76f3aee
liangfutang <tangliangfucom@sina.com>
2018-09-12 18:23:32 +0800

    dubbo空指针异常的修复，没有为itemCatService添加@autowriter注入导致了空指针异常

--- mall-manager-web/src/main/java/com/zjut/mall/controller/ItemCatController.java
+++ mall-manager-web/src/main/java/com/zjut/mall/controller/ItemCatController.java
@@ -2,6 +2,7 @@ package com.zjut.mall.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.zjut.mall.service.ItemCatService;
 @RequestMapping
 public class ItemCatController {
 
+	@Autowired
 	private ItemCatService itemCatService;
 	
 	@RequestMapping("/item/cat/list")
